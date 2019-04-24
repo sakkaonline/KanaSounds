606cf5ab88e15744fc964b83c6f08d8763cf919b
Sayaka Shiina <sakkaonline@gmail.com>
2019-04-24 22:45:56 +0900

    Chart1に清音のボタンを配列し、カスタムボタン用のxmlを用意

--- app/src/main/java/jp/sakkaonline/kanasounds/MainActivity.kt
+++ app/src/main/java/jp/sakkaonline/kanasounds/MainActivity.kt
@@ -121,16 +121,16 @@ class MainActivity : AppCompatActivity() {
         val mStartButton1 = findViewById<View>(R.id.start_button1) as Button
         mStartButton1.setOnClickListener { val intent = Intent(this, Chart1Activity::class.java)
             startActivity(intent) }
-        val mStartButton2 = findViewById<View>(R.id.start_button2) as Button
+        val mStartButton2 = findViewById<Button>(R.id.start_button2) as Button
         mStartButton2.setOnClickListener { val intent = Intent(this, Chart2Activity::class.java)
             startActivity(intent) }
-        val mStartButton3 = findViewById<View>(R.id.start_button3) as Button
+        val mStartButton3 = findViewById<Button>(R.id.start_button3) as Button
         mStartButton3.setOnClickListener { val intent = Intent(this, Chart3Activity::class.java)
             startActivity(intent) }
-        val mStartButton4 = findViewById<View>(R.id.start_button4) as Button
+        val mStartButton4 = findViewById<Button>(R.id.start_button4) as Button
         mStartButton4.setOnClickListener { val intent = Intent(this, Chart4Activity::class.java)
             startActivity(intent) }
-        val mStartButton5 = findViewById<View>(R.id.start_button5) as Button
+        val mStartButton5 = findViewById<Button>(R.id.start_button5) as Button
         mStartButton5.setOnClickListener { val intent = Intent(this, Chart5Activity::class.java)
             startActivity(intent) }
     }
