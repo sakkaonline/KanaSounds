ca22275ae3a418925c338c63aa9ae675eef50361
Sayaka Shiina <sakkaonline@gmail.com>
2019-04-27 21:35:09 +0900

    Set all charts in chart1 to 6 xmls.

---
+++ app/src/main/java/jp/sakkaonline/kanasounds/Chart6Activity.kt
@@ -0,0 +1,12 @@
+package jp.sakkaonline.kanasounds
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Chart6Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chart6)
+    }
+}
